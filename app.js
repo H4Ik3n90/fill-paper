09c62516ebb4bcdea939962ef1a539cdd687141b
H4Ik3n90 <daffakhayru@gmail.com>
2022-08-30 21:00:59 +0700

    add some file

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const router = require('./routes/UserRoute');
+
 const app = express();
+
+app.use(router);
 
 app.listen(3000 ,() => {
     console.log("server running at http://localhost:3000");
