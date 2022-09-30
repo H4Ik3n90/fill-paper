04bb1170fd2af975b99c4cb2d97c9e45a568ca41
H4Ik3n90 <daffakhayru@gmail.com>
2022-09-30 20:34:34 +0700

    updates some changes

---
+++ controller/userController.js
@@ -0,0 +1,67 @@
+const { prisma } = require('../model/UserModel.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const user_fill_paper = "FILL_PAPER_ACCESS"
+
+console.log('h')
+
+const register = async (req,res) => {
+    const {id,username,email,password} = req.body;
+    
+    const usernameReady = await prisma.user.findUnique({where: {username: username}});
+    const emailReady = await prisma.user.findUnique({where: {email: email}});
+
+    if(emailReady && usernameReady) {
+        return res.status(400).json({message: "Email and username aleread taken"});
+    } if(emailReady) {
+        return res.status(400).json({message: "Email has already taken"});
+    } if(usernameReady) {
+        return res.status(400).json({message: "Username already taken"});
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const signup = await prisma.user.create({
+        data: {
+            id: id,
+            username: username,
+            email: email,
+            password: hashPassword
+        }
+    });
+
+    const userToken = jwt.sign({id: signup.id, username: signup.username, email: signup.email, password: signup.password}, user_fill_paper);
+
+    res.status(201).json({user: signup, token: userToken});
+};
+
+const getUser = async (req,res) => {
+    const userData = await prisma.user.findMany();
+
+    res.json(userData);
+};
+
+const postNote = () => {
+
+};
+
+const getAllNote = () => {
+
+};
+
+const updateNote = () => {
+
+};
+
+const deleteNote = () => {
+
+};
+
+module.exports = {
+    register,
+    getUser,
+    getAllNote,
+    postNote,
+    updateNote,
+    deleteNote
+}
