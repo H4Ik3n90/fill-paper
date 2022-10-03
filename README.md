d6323a4c38a7c3b3b9c0552028292a6423344fb6
H4Ik3n90 <daffakhayru@gmail.com>
2022-10-03 09:46:33 +0700

    update files changes

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-<p align="center">
-    <img width="200" height="200" src="./assets/fill_paper_logo.svg">
-    <img width="200" height="200" src="./assets/fill_paper_text.svg">
-</p>
+
 
 <font size="3">
     Fill Paper is note editor project using MERN stack (mysql,express,react js,node js), using sequelize for orm can be easy to create and manage SQL database without knowing sql language
--- client/src/Pages/loginPages/Login.jsx
+++ client/src/Pages/loginPages/Login.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState,useEffect,useRef} from 'react';
 import { Link,useLocation } from 'react-router-dom';
 
 const Login = () => {
+    const [isEmail,setIsEmail] = useState();
+    const [isPass,setIsPass] = useState();
+
+    const emailHidden = (e) => {
+        e.target.value.length === 0 ? setIsEmail('') : setIsEmail('hidden');
+    };
+
+    const pwdHidden = (e) => {
+        e.target.value.length === 0 ? setIsPass('') : setIsPass('hidden');
+    };
+
     return (
         <div className='flex-1'>
             <h1 className='text-3xl text-center pt-20 pb-2'>Welcome</h1>
             <p className='text-center pb-2 text-silver'>Write your notes anywhere and anytime</p>
-            <form className='flex flex-col pt-[2%] items-center'>
-                <input type="text" className='border-2 h-10 rounded bg-blue-smooth focus-within:outline-blue-md w-80 mb-6'/>
-                <input type="text" className='border-2 h-10 rounded bg-blue-smooth focus-within:outline-blue-md w-80 mb-10'/>
+            <form className='flex flex-col justify-around pt-[2%] items-center h-[33%]'>
+                <div className='relative h-auto'>
+                    <input type="text" className={`border-1 h-10 rounded bg-blue-smooth pl-4 focus-within:outline-blue-md w-80`} 
+                        onInput={(e) => emailHidden(e)}
+                    />
+                    <label className={`${isEmail} absolute duration-350 left-4 top-2 text-md text-silver`}>Email</label>
+                </div>
+                <div className='relative h-auto'>
+                    <input type="text" className={`border-1 h-10 rounded bg-blue-smooth pl-4 focus-within:outline-blue-md w-80`} 
+                        onInput={(e) => pwdHidden(e)}
+                    />
+                    <label className={`${isPass} absolute duration-350 left-4 top-2 text-md text-silver`}>Password</label>
+                </div>
                 <button className='border border-blue-md bg-blue-md p-1 text-lg text-white w-80 rounded'>Login</button>
                 
             </form>
